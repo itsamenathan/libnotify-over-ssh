77583c295cd702c5b296190625bd70df4e6b809d
Nathan W <nathan@frcv.net>
2011-08-31 20:22:23 -0500

    Chaned the server to a non forking version.  I'm  not sure why I was forking.  Hopefully this will be a little cleaner.

--- client_notify.pl
+++ client_notify.pl
@@ -1,27 +1,26 @@
 #!/usr/bin/perl -w
+use IO::Socket::INET;
 use strict;
-use IO::Socket;
+
+my $HOST = '127.0.0.1';
+my $PORT = '1216';
 
 my $tag = shift || $ARGV[0];
 my $title = shift || $ARGV[1];
 my $summary  = shift || $ARGV[2];
 
-my $HOST = '127.0.0.1';
-my $PORT = '1216';
 
-my $sock = new IO::Socket::INET(
-    PeerAddr    => $HOST,
-    PeerPort    => $PORT,
-    Proto        => 'tcp',
-) or exit;
- 
+my $sock = IO::Socket::INET->new('PeerAddr' => $HOST,
+    'PeerPort' => $PORT,
+    'Proto' => 'tcp')
+or exit;
+
 $sock->autoflush(1);
- 
-#print $sock "\"$title\" \"$summary\"\n";
+
 if($tag && $title && $summary){
   print $sock $tag."\n";
   print $sock $title."\n";
   print $sock $summary."\n";
 }
 
-close $sock;
+close $sock
--- server_notify.pl
+++ server_notify.pl
@@ -1,47 +1,35 @@
 #!/usr/bin/perl -w
 use strict;
 use IO::Socket;
-sub message;
 
-$SIG{CHLD} = 'IGNORE'; #automatic child reaping
-
-my $weechat_icon='/home/jimshoe/dev/libnotify-over-ssh/weechat_icon.png';
+my $weechat_icon='/home/nwarner/scripts/libnotify-over-ssh/weechat_icon.png';
 my $HOST = '127.0.0.1';
 my $PORT = '1216';
 
-my $sock = new IO::Socket::INET(
-    LocalHost    => $HOST,
-    LocalPort    => $PORT,
-    Proto        => 'tcp',
-    Listen        => SOMAXCONN,
-    Reuse        => 1
-    ) or die "no socket :$!";
-my ($new_sock, $c_addr, $buf);
-$sock->autoflush(1);
-while (($new_sock, $c_addr) = $sock->accept()) {
-  my ($client_port, $c_ip) = sockaddr_in($c_addr);
-  my $client_ipnum = inet_ntoa($c_ip);
-  my $client_host =gethostbyaddr($c_ip, AF_INET);
-  my $pid = fork();
-  print "Cannot fork : $!\n" unless (defined($pid));
 
-  if ($pid == 0) {
-    my ($tag, $title, $summary,$x);
-    while (defined ($buf = <$new_sock>)) {
-      chomp $buf;
-      $x++;
-      if($x == 1){$tag = $buf;}
-      if($x == 2){$title = $buf;}
-      if($x == 3){
-        $summary = $buf;
-        message($tag,$title,$summary);
-      }
+my $socket = IO::Socket::INET->new('LocalPort' => $PORT,
+    'Proto' => 'tcp',
+    'Listen' => 4)
+or die "Can't create socket ($!)\n";
+print "Server listening\n";
+while (my $client = $socket->accept) {
+  my ($tag, $title, $summary,$x);
+  while (<$client>) {
+    my $buf = $_;
+    chomp $buf;
+    $x++;
+    if($x == 1){$tag = $buf;}
+    if($x == 2){$title = $buf;}
+    if($x == 3){
+      $summary = $buf;
+      message($tag,$title,$summary);
     }
-    exit(0);
   }
+  close $client
+    or die "Can't close ($!)\n";
 }
 
-sub message ($$$) {
+sub message{
   my $tag = $_[0];
   my $title = $_[1];
   my $summary = $_[2];
@@ -52,13 +40,13 @@ sub message ($$$) {
       $title =~ s/[<&]//g; # remove some characters that libnotify hates
       $summary =~ s/[<&]//g;
       my @args = ('/usr/bin/notify-send', $title, $summary, "--icon=$weechat_icon");
-      exec @args;
+      system @args;
     }
   }
   if($tag eq 'system'){
     $title =~ s/[<&]//g; # remove some characters that libnotify hates
     $summary =~ s/[<&]//g;
     my @args = ('/usr/bin/notify-send', $title, $summary);
-    exec @args;
+    system @args;
   }
 }
